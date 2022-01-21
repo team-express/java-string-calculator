ab6f5078fd459976a8ab48cfaa225cc45886b3f9
sonchanwoo <chanchan1116@naver.com>
2022-01-21 17:31:30 +0900

    feat: add isAllNumber()

--- src/main/java/checker/ValidStringExpressionChecker.java
+++ src/main/java/checker/ValidStringExpressionChecker.java
@@ -4,4 +4,13 @@ public class ValidStringExpressionChecker {
 	private static boolean isNumber(char c) {
 		return c >= '0' && c <= '9';
 	}
+
+	public static boolean isAllNumber(String s) {
+		int i;
+
+		for (i = 0; i < s.length() && isNumber(s.charAt(i)); i++)
+			;
+
+		return i == s.length();
+	}
 }
