a3bd707d846706adf7a9235c8499aed8fd0f76d7
sonchanwoo <chanchan1116@naver.com>
2022-01-21 20:02:51 +0900

    feat: Add check()

--- src/main/java/checker/ValidStringExpressionChecker.java
+++ src/main/java/checker/ValidStringExpressionChecker.java
@@ -1,5 +1,7 @@
 package checker;
 
+import java.util.Arrays;
+
 public class ValidStringExpressionChecker {
 
 	private static boolean isNumber(char c) {
@@ -17,7 +19,29 @@ public class ValidStringExpressionChecker {
 	}
 
 	private static boolean isOneOperator(String s) {
+
+		if (s.length() != 1)
+			return false;
+
 		char c = s.charAt(0);
-		return s.length() == 1 && (c == '+' || c == '*' || c == '-' || c == '/');
+
+		return c == '+' || c == '*' || c == '-' || c == '/';
+	}
+
+	public static boolean check(String s) {
+
+		String[] splitExpression = s.trim().split(" ");
+
+		int numberIndex;
+		for (numberIndex = 0;
+			 numberIndex < splitExpression.length && isAllNumber(splitExpression[numberIndex]); numberIndex += 2)
+			;
+
+		int operatorIndex;
+		for (operatorIndex = 1; operatorIndex < splitExpression.length && isOneOperator(
+			splitExpression[operatorIndex]); operatorIndex += 2)
+			;
+
+		return numberIndex == splitExpression.length+1 && operatorIndex == splitExpression.length;
 	}
 }
