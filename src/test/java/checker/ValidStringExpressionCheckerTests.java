1188df627124e567f85d085c708368cc59434ad2
sonchanwoo <chanchan1116@naver.com>
2022-01-21 17:10:30 +0900

    test: add smaller unit test, modify bigger unit test

--- src/test/java/checker/ValidStringExpressionCheckerTests.java
+++ src/test/java/checker/ValidStringExpressionCheckerTests.java
@@ -11,10 +11,19 @@ public class ValidStringExpressionCheckerTests {
 
 	@Test
 	@DisplayName("주어진 문자열이 숫자인가")
-	void testIsNumber() {
-		assertThat(ValidStringExpressionChecker.isNumber("333")).isTrue;
-		assertThat(ValidStringExpressionChecker.isNumber("3 ")).isFalse;
-		assertThat(ValidStringExpressionChecker.isNumber(" 3")).isFalse;
-		assertThat(ValidStringExpressionChecker.isNumber("33a")).isFalse;
+	void testIsAllNumber() {
+		assertThat(ValidStringExpressionChecker.isAllNumber("333")).isTrue;
+		assertThat(ValidStringExpressionChecker.isAllNumber("3 ")).isFalse;
+		assertThat(ValidStringExpressionChecker.isAllNumber(" 3")).isFalse;
+		assertThat(ValidStringExpressionChecker.isAllNumber("33a")).isFalse;
+	}
+
+	@Test
+	@DisplayName("주어진 한 문자가 숫자인가")
+	void testIsNumber(){
+		assertThat(ValidStringExpressionChecker.isNumber('3')).isTrue();
+		assertThat(ValidStringExpressionChecker.isNumber('+')).isFalse();
+		assertThat(ValidStringExpressionChecker.isNumber(' ')).isFalse();
+		assertThat(ValidStringExpressionChecker.isNumber('a')).isFalse();
 	}
 }
