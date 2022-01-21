19c696411c03db44e6bb15ff2c1bc2deba8ed168
sonchanwoo <chanchan1116@naver.com>
2022-01-21 18:08:49 +0900

    test: Add testCheck()

--- src/test/java/checker/ValidStringExpressionCheckerTests.java
+++ src/test/java/checker/ValidStringExpressionCheckerTests.java
@@ -8,4 +8,13 @@ import static org.assertj.core.api.Assertions.assertThat;
 import java.util.Arrays;
 
 public class ValidStringExpressionCheckerTests {
+
+	@Test
+	@DisplayName("주어진 문자열이 수식이 될 수 있는지 검사한다. (홀수자리가 숫자, 짝수자리가 (하나의) 연산자)")
+	void testCheck(){
+		assertThat(ValidStringExpressionChecker.check(" 2 + 3 / 5")).isEqualTo(true);
+		assertThat(ValidStringExpressionChecker.check("2 + 3  / 5")).isEqualTo(false);
+		assertThat(ValidStringExpressionChecker.check(" 2 + 3 /5")).isEqualTo(false);
+		assertThat(ValidStringExpressionChecker.check(" 2 + 33 / 5")).isEqualTo(true);
+	}
 }
