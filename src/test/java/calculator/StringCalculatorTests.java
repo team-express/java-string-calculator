3d069add6f3dfb8a11f21575b57b6b973fac0864
sonchanwoo <chanchan1116@naver.com>
2022-01-21 15:12:49 +0900

    test: add testCalculate()

--- src/test/java/calculator/StringCalculatorTests.java
+++ src/test/java/calculator/StringCalculatorTests.java
@@ -6,4 +6,13 @@ import org.junit.jupiter.api.Test;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class StringCalculatorTests {
+	@Test
+	@DisplayName("split으로 인하여 나누어진 String[]타입의 수식을 계산하여 반환한다.")
+	void testCalculate() {
+		String[] arr = "2 + 3 * 4 / 2".split(" ");
+
+		int result = StringCalculator.calculate(arr);
+
+		assertThat(result).isEqualTo(10);
+	}
 }
