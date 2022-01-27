ff16cfb12a7d6fb98ffd7bbcb7b0c6a852091826
sonchanwoo <chanchan1116@naver.com>
2022-01-27 11:12:21 +0900

    feat: Add makeExpression(), need smaller unit

--- src/main/java/expression/ExpressionBuilder.java
+++ src/main/java/expression/ExpressionBuilder.java
@@ -1,7 +1,18 @@
 package expression;
 
+import checker.ValidStringExpressionChecker;
+
 public class ExpressionBuilder {
-	public static Expression makeExpression(String input) {
-		return null;
+	public static Expression makeExpression(String stringExpression) {
+		Expression expression = null;
+
+		String[] splitStringExpression = stringExpression.trim().split(" ");
+
+		if(ValidStringExpressionChecker.check(splitStringExpression)){
+			Object[] splitExpression = method();//어떤 메서드
+			expression = new Expression(splitExpression);
+		}
+
+		return expression;
 	}
 }
