ba5395b7818401aa0990ddbfdb0e30c04604dbd2
sonchanwoo <chanchan1116@naver.com>
2022-01-27 17:31:05 +0900

    feat: Add main

---
+++ src/main/java/CalStringGame/Main.java
@@ -0,0 +1,23 @@
+package CalStringGame;
+
+import java.util.Scanner;
+
+import view.InputView;
+import view.OutputView;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		InputView inputView = new InputView(scanner);
+		OutputView outputView = new OutputView();
+		GameManager manager = new GameManager();
+
+		start(inputView, outputView, manager);
+	}
+
+	private static void start(InputView inputView, OutputView outputView, GameManager manager) {
+		manager.init(inputView.read());
+		manager.play();
+		outputView.print(manager.getResult());
+	}
+}
