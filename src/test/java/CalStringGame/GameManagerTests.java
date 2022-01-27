cc621aa925b214c8a7b501989f761d9672ca08e6
sonchanwoo <chanchan1116@naver.com>
2022-01-27 16:50:53 +0900

    test: Add test

---
+++ src/test/java/CalStringGame/GameManagerTests.java
@@ -0,0 +1,26 @@
+package CalStringGame;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class GameManagerTests {
+	private GameManager manager;
+
+	@BeforeEach
+	@DisplayName("")
+	void setUp() {
+		manager = new GameManager();
+		manager.init(" 2 + 3 * 4 / 2 ");
+	}
+
+	@Test
+	@DisplayName("")
+	void test() {
+		manager.play();
+		assertThat(manager.getResult()).isEqualTo(10);
+	}
+
+}
