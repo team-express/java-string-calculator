404c9864c25bd2141099954030957598f8397687
sonchanwoo <chanchan1116@naver.com>
2022-01-27 16:42:02 +0900

    feat: Add class InputView

---
+++ src/main/java/view/InputView.java
@@ -0,0 +1,15 @@
+package view;
+
+import java.util.Scanner;
+
+public class InputView {
+	private final Scanner scanner;
+
+	public InputView(Scanner scanner){
+		this.scanner = scanner;
+	}
+
+	public int read(){
+		return scanner.nextInt();
+	}
+}
