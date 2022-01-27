22f68359d833c9053c2e5de55bf0142d4844e371
sonchanwoo <chanchan1116@naver.com>
2022-01-27 10:58:30 +0900

    feat: Add class Expression

--- src/main/java/expression/Expression.java
+++ src/main/java/expression/Expression.java
@@ -1,4 +1,18 @@
 package expression;
 
+import java.util.Arrays;
+
 public class Expression {
+	private Object[] expression;
+
+	Expression(Object[] expression){//package
+		this.expression = expression;
+	}
+
+	@Override
+	public String toString() {
+		return "Expression{" +
+			"expression=" + Arrays.toString(expression) +
+			'}';
+	}
 }
