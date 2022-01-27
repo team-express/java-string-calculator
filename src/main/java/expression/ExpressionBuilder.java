00562fb2499daaddc7af3a774684b72b9a713e9b
sonchanwoo <chanchan1116@naver.com>
2022-01-27 11:40:54 +0900

    feat: Modify makeExpression, need to change param of validStringExpressionChecker.check()

--- src/main/java/expression/ExpressionBuilder.java
+++ src/main/java/expression/ExpressionBuilder.java
@@ -8,11 +8,21 @@ public class ExpressionBuilder {
 
 		String[] splitStringExpression = stringExpression.trim().split(" ");
 
-		if(ValidStringExpressionChecker.check(splitStringExpression)){
-			Object[] splitExpression = method();//어떤 메서드
+		if(ValidStringExpressionChecker.check(splitStringExpression)){//ValidStringExpressionChecker 수정요망
+			Object[] splitExpression = getSplitExpression(splitStringExpression);
 			expression = new Expression(splitExpression);
 		}
 
 		return expression;
+	}
+
+	private static Object[] getSplitExpression(String[] splitStringExpression){
+		Object[] splitExpression = splitStringExpression;
+
+		for(int i=0;i<splitExpression.length;i+=2){
+			splitExpression[i] = Integer.parseInt(splitStringExpression[i]);
+		}
+
+		return splitExpression;
 	}
 }
