ab3c180bfa01ef71c2110c4e8d58e9f5cd4a282d
sonchanwoo <chanchan1116@naver.com>
2022-02-08 10:40:49 +0900

    Refactor: Remove worthless local var

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -4,12 +4,11 @@ public class OutputView {
 	private static final String ERROR_MESSAGE = "입력 값이 없거나, 잘못된 형식의 수식입니다.";
 
 	public void print(String result) {
-		String message = result;
 
-		if (message == null) {
-			message = ERROR_MESSAGE;
+		if (result == null) {
+			result = ERROR_MESSAGE;
 		}
 
-		System.out.println(message);
+		System.out.println(result);
 	}
 }
