bb857a9b7fe688f986842fa988177836dd5fd21a
sonchanwoo <chanchan1116@naver.com>
2022-02-08 12:17:09 +0900

    test: Modify test-code

--- src/test/java/calstringgame/GameManagerTests.java
+++ src/test/java/calstringgame/GameManagerTests.java
@@ -10,17 +10,13 @@ public class GameManagerTests {
 	private GameManager manager;
 
 	@BeforeEach
-	@DisplayName("")
 	void setUp() {
 		manager = new GameManager();
-		manager.init(" 2 + 3 * 4 / 2 ");
 	}
 
 	@Test
-	@DisplayName("")
 	void test() {
-		manager.play();
-		assertThat(manager.getResult()).isEqualTo("10");
+		assertThat(manager.play(" 2 + 3 * 4 / 2 ")).isEqualTo("10");
 	}
 
 }
