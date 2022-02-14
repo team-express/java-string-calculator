9f7d37bb5a267657ee016321a2c33759e42a0d80
Jaemin Kim <jaeminkim90@gmail.com>
2022-02-14 18:36:01 +0900

    FEAT: 계산의 재료가 되는 숫자와 연산자를 넘기면 전체 계산 결과를 반환하는 계산기 클래스 구현

---
+++ src/main/java/step2/Calculator.java
@@ -0,0 +1,55 @@
+package step2;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Calculator {
+
+    private static final int NUMBERS_MIN_SIZE = 2;
+    private static final int OPERATORS_MIN_SIZE = 1;
+    private static final int NUMBERS_AND_OPERATORS_INTERVAL = 1;
+
+    private final List<Integer> numbers; // 숫자만 모아놓은 List
+    private final List<Operator> operators; // 연산자만 모아놓은 List
+
+    // 계산기 인스턴스는 생성자를 이용해 계산의 재료가 되는 숫자와 연산자 모음을 필수로 전달 받는다.
+    public Calculator(List<Integer> numbers, List<Operator> operators) {
+        validateMinSize(numbers, operators); // 연산에 필요한 최소 조건을 검증
+        validateFormat(numbers, operators); // 연산자의 갯수가 숫자의 갯수보다 1개 작은지 검증
+        this.numbers = Collections.unmodifiableList(numbers); // Read-Only 처리하여 필드로 넘긴다.
+        this.operators = Collections.unmodifiableList(operators); // Read-Only 처리하여 필드로 넘긴다.
+    }
+
+    // 계산을 수행할 수 있는 정수와 연산자의 최소 조건(갯수) 검증
+    private void validateMinSize(List<Integer> numbers, List<Operator> operators) {
+        if (numbers.size() < NUMBERS_MIN_SIZE) {
+            // 계산의 대상이 되는 숫자가 2개보다 작을 경우 예외 발생
+            throw new IllegalArgumentException(String.format("방정식의 숫자의 수는 %d개 이상이어야 합니다.", NUMBERS_MIN_SIZE));
+        }
+
+        if (operators.size() < OPERATORS_MIN_SIZE) {
+            // 계산에 사용되는 연산자가 없을 경우 예외 발생
+            throw new IllegalArgumentException(String.format("방정식의 연산자의 수는 %d개 이상이어야 합니다.", OPERATORS_MIN_SIZE));
+        }
+    }
+
+    // 숫자와 연산자의 갯수 차이를 통해 정상적인 계산이 가능한지 검증
+    private void validateFormat(List<Integer> numbers, List<Operator> operators) {
+        int interval = numbers.size() - operators.size();
+        if (interval != NUMBERS_AND_OPERATORS_INTERVAL) {
+            // (숫자 - 연산자)의 수가 1이 아닐 경우 예외 발생
+            throw new IllegalArgumentException("방정식의 올바른 형식이 아닙니다.");
+        }
+    }
+
+    // Calculator 클래스가 상태로 가지고 있는 숫자와 연산자의 통합 계산을 수행하고, 결과를 반환한다.
+    public int calculate() {
+        int result = numbers.get(0); // 첫번째 숫자를 result에 넣어 계산 초기값을 준비한다
+        for (int i = 0; i < operators.size(); i++) { // 연산자의 갯수만큼 연산을 반복한다
+            Operator operator = operators.get(i);// 순차적으로 연산자 enum 객체를 찾는다
+            Integer number = numbers.get(i + 1); // 연산의 대상이 되는 숫자를 number에 셋팅한다
+            result = operator.calculate(result, number); // 두 수를 계산하여 중간 계산 결과를 result에 담는다
+        }
+        return result;
+    }
+}
---
+++ src/test/java/step2/CalculatorTest.java
@@ -0,0 +1,104 @@
+package step2;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+// BDD 테스트 코드 작성 패턴
+// Describe : 테스트 대상을 명사로 작성
+// Context : ~인 경우, ~ 할 때, 만약 ~ 한다면과 같이 상황 또는 조건을 기술한다
+// It : 위에서 명사로 작성한 테스트 대상의 행동을 ~이다, ~한다, ~를 갖는다 등으로 작성한다
+@DisplayName("Calculator 클래스 동작 시나리오 테스트")
+class CalculatorTest {
+
+    @Nested
+    @DisplayName("Describe: Calculator 생성자는")
+    class Describe_constructor {
+
+        // subject를 이용해 테스트 대상이 되는 메서드를 분리한다.
+        // Calculator 생성자는 숫자와 연산자 모음을 받아, 상태로 가지고 있는 인스턴스를 만든다.
+        private Calculator subject(List<Integer> numbers, List<Operator> operators) {
+            return new Calculator(numbers, operators);
+        }
+
+        @Nested
+        @DisplayName("Context: 최소 두 개의 숫자와 하나의 연산자가 넘어올 경우")
+        class Context_two_number_and_one_operator {
+            @Test
+            @DisplayName("It: 계산기 객체를 반환한다.")
+            void it_returns_a_equation() {
+                assertNotNull(subject(Arrays.asList(1, 2), Arrays.asList(Operator.SUM)));
+                assertThat(subject(Arrays.asList(1, 2), Arrays.asList(Operator.SUM))).isInstanceOf(Calculator.class);
+            }
+        }
+
+        @Nested
+        @DisplayName("Context: 한 개의 숫자와 한 개의 연산자만 넘어올 경우")
+        class Context_one_number {
+            @Test
+            @DisplayName("It: 예외가 발생한다")
+            void it_throws_exception() {
+                assertThrows(IllegalArgumentException.class, () -> {
+                   subject(Collections.singletonList(1), Collections.singletonList(Operator.SUM));
+                });
+            }
+        }
+
+        @Nested
+        @DisplayName("Context: 연산자가 넘어오지 않을 경우")
+        class Context_empty_operator {
+            @Test
+            @DisplayName("It: 예외가 발생한다")
+            void it_throws_exception() {
+                assertThrows(IllegalArgumentException.class, () -> {
+                    subject(Arrays.asList(1, 2), Collections.emptyList());
+                });
+            }
+        }
+
+        @Nested
+        @DisplayName("Context: 숫자 모음과 연산자 모음의 갯수 차이가 1이 아닐 경우")
+        class Context_wrong_format {
+            @Test
+            @DisplayName("It: 예외가 발생한다")
+            void it_throws_exception() {
+                assertThrows(IllegalArgumentException.class, () -> {
+                    subject(Arrays.asList(1, 2, 3), Collections.singletonList(Operator.SUM));
+                });
+            }
+        }
+    }
+
+    @Nested
+    @DisplayName("Describe: calculate 메소드는")
+    class Describe_calculate {
+
+        // subject를 이용해 테스트 대상이 되는 메서드를 분리한다.
+        // Calculator 인스턴스의 calculate()를 실행한다.
+        private int subject(List<Integer> numbers, List<Operator> operators) {
+            Calculator calculator = new Calculator(numbers, operators);
+            return calculator.calculate();
+        }
+
+        @Nested
+        @DisplayName("Context: 정상적인 숫자 List와 연산자 List가 필드에 있을 경우")
+        class Context_with_numbers_and_operators {
+
+            @Test
+            @DisplayName("It: calculator를 이용해 전체 계산 결과를 반환한다")
+            void it_returns_a_result() {
+                int calculateResult = subject(Arrays.asList(1, 2, 3), Arrays.asList(Operator.SUM, Operator.DIVISION));
+                assertThat(calculateResult).isEqualTo(1);
+            }
+        }
+    }
+}
+
+
